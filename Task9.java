b3f467aa254c0ce9bbf17338b2309dccb4172441
mariambarbolina <159700121+mariambarbolina@users.noreply.github.com>
2024-02-18 22:30:37 +0300

    Update Task9.java

--- Task9.java
+++ Task9.java
@@ -1,27 +1,27 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 public class Task9 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        char b;
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random r = new Random();
-        char c, answer = alphabet.charAt(r.nextInt(alphabet.length()));
+        char c = alphabet.charAt(r.nextInt(alphabet.length()));
         System.out.println(c);
+        System.out.println("Задумана буква от А до Z, попытайтесь ее угадать: ");
+        do{
 
-        System.out.println("Задумана буква от А до Z");
-        System.out.print("Попытайтесь ее угадать: ");
-        char b = in.next().charAt(0);
-        while ((b = (char)System.in.read()) != answer) {
-            System.out.print("Не угадал, ");
-
-            if (ch < answer) System.out.println("нужная буква ближе к концу алфавита");
-            else System.out.println("нужная буква ближе к началу алфавита");
-
-            System.out.print("Вводи еще: ");
-            System.in.read();
+            b = in.next().charAt(0);
+            if (b < c) {
+                System.out.println("Нужная буква ближе к концу алфавит, вводи еще: ");
+                continue;
+            }
+            else if (b > c){
+                System.out.println("нужная буква ближе к началу алфавита, вводи еще: ");
+                continue;
+            }
         }
+        while (b != c);
         System.out.print("** Правильно! **");
-
     }
 }
